14531193947d7f8c1259f217fecbe1138a4b4d1f
Xanthus <iamxanthus@gmail.com>
2026-05-28 12:10:20 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+// Import required modules
+const express = require('express');
+
+// Create an Express application
+const app = express();
+
+// Set EJS as the view engine
+app.set('view engine', 'ejs');
+
+// Middleware to parse request bodies
+app.use(express.urlencoded({ extended: true }));
+
+//Define a route to render the index page
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+// Start the server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port http://localhost:${PORT}`);
+});
